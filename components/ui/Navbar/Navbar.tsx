2f6c6e1d3b7d217246ba29f860abacf58070e6da
mattbratos <mattbratos@gmail.com>
2023-11-18 21:16:18 +0000

    pnpm install to update the pnpm-lock.yaml
    wheteveer da fuck it means

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { createServerSupabaseClient } from '@/app/supabase-server';
 
-import Logo from '@/components/icons/Logo';
 import SignOutButton from './SignOutButton';
 
 import s from './Navbar.module.css';
@@ -17,11 +16,11 @@ export default async function Navbar() {
       <a href="#skip" className="sr-only focus:not-sr-only">
         Skip to content
       </a>
-      <div className="max-w-6xl px-6 mx-auto">
-        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+      <div className="px-6">
+        <div className="relative flex flex-row justify-between py-4 align-center md:py-8">
           <div className="flex items-center flex-1">
-            <Link href="/" className={s.logo} aria-label="Logo">
-              <Logo />
+            <Link href="/" className="w-48">
+              <img src="/logofinal.png" alt="Logo" />
             </Link>
             <nav className="hidden ml-6 space-x-2 lg:block">
               <Link href="/" className={s.link}>
