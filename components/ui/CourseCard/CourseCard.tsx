eccef45d1dc61789c74c25e10a993b81d9ec4884
mattbratos <mattbratos@gmail.com>
2023-11-19 01:07:35 +0000

    class changed to className

--- components/ui/CourseCard/CourseCard.tsx
+++ components/ui/CourseCard/CourseCard.tsx
@@ -16,17 +16,17 @@ interface CourseCardProps {
 const CourseCard: React.FC<CourseCardProps> = ({ title, college, rating, description, duration, studentCount, imageSrc }) => {
     return (
     <div className="course-card border-2 rounded-xl border-[#FF00FF]">
-      <img src={imageSrc} alt="Description" class="rounded-t-xl" />
-      <div class="p-4">
-        <div class="flex items-center justify-between">
-        <h3 class="font-extrabold text-white sm:text-3xl">{title}</h3>
+      <img src={imageSrc} alt="Description" className="rounded-t-xl" />
+      <div className="p-4">
+        <div className="flex items-center justify-between">
+        <h3 className="font-extrabold text-white sm:text-3xl">{title}</h3>
           <p>⭐️{rating}</p>
         </div>
-        <p class="text-white sm:text-lg mt-6" >{description}</p>
-        <div class="mt-4">
+        <p className="text-white sm:text-lg mt-6" >{description}</p>
+        <div className="mt-4">
             <p>⏳ {duration}</p>
         </div>
-        <div class="flex items-center justify-between mt-2 ">
+        <div className="flex items-center justify-between mt-2 ">
             <p>🏛️ {college}</p>
             <p>👨‍🎓 {studentCount}</p>
         </div>
